7a46580198246f58925d9fe6cf728f593488c515
Philipp Hug <philipp@hug.cx>
2020-08-05 15:12:12 +0200

    add test

--- pkg/trustlists/trust_test.go
+++ pkg/trustlists/trust_test.go
@@ -1,1 +1,14 @@
 package trustlists
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestEUCertPool(t *testing.T) {
+	p, err := EUCertPool(Options{})
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Println(p)
+}
